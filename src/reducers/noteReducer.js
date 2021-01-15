98ea819b6ec308f4410601dec60cf54d10b4cd17
Juan33 <58494935+juansolares333@users.noreply.github.com>
2021-01-14 20:54:56 -0600

    commit

---
+++ src/reducers/noteReducer.js
@@ -0,0 +1,35 @@
+/*
+    {
+        notes: [],
+        active: null,
+        active: {
+            id: 'id by firebase',
+            title: '',
+            body: '',
+            imageUrl: '',
+            date: 232323
+        }
+    }
+*/
+
+import { types } from "../types/types";
+
+const initialState = {
+  notes: [],
+  active: null,
+};
+
+export const notesReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case types.notesActive:
+      return {
+        ...state,
+        active: {
+          ...action.payload,
+        },
+      };
+
+    default:
+      return state;
+  }
+};
--- src/types/types.js
+++ src/types/types.js
@@ -1,8 +1,18 @@
 export const types = {
+  //login and logout
   login: "[Auth] Login",
   logout: "[Auth] Logout",
+  //ui
   uiSetError: "[UI] Set error",
   uiRemoveError: "[UI] Remove error",
   uiStartLoading: "[UI] Start loading",
   uiFinishLoading: "[UI] Finish loading",
+  //notes
+  notesAddNew: "[Notes] New note",
+  notesActive: "[Notes] Set active note",
+  notesLoad: "[Notes] Load notes",
+  notesUpdated: "[Notes] Update note",
+  notesFileUrl: "[Notes] Update image url",
+  notesDelete: "[Notes] Delete note",
+  notesLoadCleaning: "[Notes] Logout cleaning",
 };
